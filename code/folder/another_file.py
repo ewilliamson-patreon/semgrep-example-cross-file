6992cf7d9b8128f7f54bd71a570e277a2a535ace
jkinsfather <139381146+jkinsfather@users.noreply.github.com>
2024-06-20 10:27:24 -0700

    Fix import in another_file.py
    The namespace for another_file.py's import statement included `code` which is not accessible from the package and causes the program to error when run.

--- code/folder/another_file.py
+++ code/folder/another_file.py
@@ -1,5 +1,5 @@
 
-from code.another_folder.dangerous_sink import dangerous_sink
+from another_folder.dangerous_sink import dangerous_sink
 
 def do_something(id: int):
     # ruleid: user_input_to_dangerous_sink
